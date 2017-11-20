83940c46ad9c22dc31b4937f4589931038298d7d
Student22 <student22@iMac22s-iMac.local>
2017-11-20 09:15:48 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/uitm/app1/lab10exercice/MainActivity.java
@@ -0,0 +1,13 @@
+package my.edu.uitm.app1.lab10exercice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
